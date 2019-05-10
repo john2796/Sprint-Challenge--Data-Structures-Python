d147c923faf50c969d2095cca9a2d3f2d54bd113
john2796 <jbmiranda22796@gmail.com>
2019-05-10 09:03:50 -0700

    Runtime Optimization

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,39 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        for _ in self.storage:
+            if self.current < len(self.storage):
+                self.storage[self.current] = item
+                self.current += 1
+            else:
+                self.current -= self.capacity
+                self.storage[self.current] = item
+                self.current += 1
+            return self.storage
 
-  def get(self):
-    pass
+    def get(self):
+        return [x for x in self.storage if x is not None]
+
+
+buffer = RingBuffer(3)
+
+print(buffer.get(), '-------> []')  # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+print(buffer.get(), '-------> a, b, c')   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer.append('d')
+print(buffer.get(), ' -------> d, b, c')   # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+print(buffer.get(), ' -------> d, e, f')  # should return ['d', 'e', 'f']
